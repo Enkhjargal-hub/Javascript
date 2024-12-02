fc295de83697ce5a6bf11afd68fed555def22aa7
Enkhjargal-hub <joloenja@gmail.com>
2024-12-02 12:06:36 +0800

    j

---
+++ Javascript/javasript.js
@@ -0,0 +1,71 @@
+// Problem 1
+
+let a = 5;
+let b = 6;
+let c = 8;
+
+console.log(a + b + c);
+
+// Problem 2
+
+let s = 8;
+let h = 9;
+
+console.log(s + h);
+
+// Problem 3
+
+let g = 5;
+
+console.log(g * g * g);
+
+// Problem 4
+
+let j = 2;
+
+console.log(4 * j * j - 3 * j + 5);
+
+// Problem 5 x-n utgiig let ogno
+
+let x = 1;
+
+console.log(3 * x - 5);
+
+// Problem 6 suuliin 6-n toog gargah gj bga
+
+let r = 476;
+
+console.log(r % 10);
+
+// Problem 7 goliin 2-n toog gargah gj bga
+
+let k = 423;
+
+let aravtiinoron = (k % 100) / 10;
+console.log(aravtiinoron);
+
+// Problem 8
+
+let l = 200;
+console.log(l / 60);
+
+// Problem 9
+
+let o = 44;
+
+console.log(o / 24);
+
+// Problem 10
+
+let m = 16;
+let n = 18;
+
+console.log(m-n)
+
+// Problem 11
+
+if (grade=5) { 
+    onts
+}
+
+
